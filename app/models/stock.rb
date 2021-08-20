dbb38222b6afda7932155b24fc2f6aa6dd4ffb48
Bryan Woodcox <bryan.woodcox@gmail.com>
2021-08-20 15:13:37 -0600

    Add user stocks resource and clean up navigation

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  validates :ticker, :name, presence: true
+
   def self.lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publishable_key],
                                   endpoint: 'https://sandbox.iexapis.com/stable')
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :user_stocks
   get 'stocks/search_stock'
   root 'welcome#index'
   devise_for :users
