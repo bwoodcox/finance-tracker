6a7a202d7121fba9c043a8e1d1f930dec1f64175
Bryan Woodcox <bryan.woodcox@gmail.com>
2021-08-19 21:40:28 -0600

    Add error checking to stock search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,16 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.lookup params[:stock]
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.lookup params[:stock]
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = "Please enter a valid stock symbol to search."
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = "Please enter a stock symbol to search."
+      redirect_to my_portfolio_path
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,10 @@ class Stock < ApplicationRecord
   def self.lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publishable_key],
                                   endpoint: 'https://sandbox.iexapis.com/stable')
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      nil
+    end
   end
 end
