c1de2c3a70b4921b90ef2e897c21d74bbf077181
Bryan Woodcox <bryan.woodcox@gmail.com>
2021-08-19 23:30:42 -0600

    dd ajax functionality for searching stocks & handling errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,14 +3,20 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.lookup params[:stock]
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
-        flash[:alert] = "Please enter a valid stock symbol to search."
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid stock symbol to search."
+          format.js { render partial: 'users/result' }
+        end
       end
     else
-      flash[:alert] = "Please enter a stock symbol to search."
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a stock symbol to search."
+        format.js { render partial: 'users/result' }
+      end
     end
   end
 end
