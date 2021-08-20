a3b60f68b6608fdd23cafeae2721c4b894c2c100
Bryan Woodcox <bryan.woodcox@gmail.com>
2021-08-19 20:14:26 -0600

    Update stock search view

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,6 @@ class Stock < ApplicationRecord
   def self.lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publishable_key],
                                   endpoint: 'https://sandbox.iexapis.com/stable')
-    client.price(ticker_symbol)
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
   end
 end
